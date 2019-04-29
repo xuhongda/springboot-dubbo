5ca652d05382a876733cd6b9d0411643d7c4ebca
xuhongda <xuhongda7@outlook.com>
2019-04-29 18:17:16 +0800

    dubbo 升级准备工作

--- springboot-consumer/src/main/java/com/xu/dubbo/SpringbootConsumerApplication.java
+++ springboot-consumer/src/main/java/com/xu/dubbo/SpringbootConsumerApplication.java
@@ -2,12 +2,13 @@ package com.xu.dubbo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ImportResource;
 
 /**
  * @author xuhongda
  */
 @SpringBootApplication
-/*@DubboComponentScan(basePackages = "com.xu.dubbo.consumer")*/
+@ImportResource("static/consumer.xml")
 public class SpringbootConsumerApplication {
 
     public static void main(String[] args) {
